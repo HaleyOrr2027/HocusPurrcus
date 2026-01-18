92699fdcccccaec5a47bf2c5f6d9efb515055570
HaleyOrr2027 <haleyorr2027@vt.edu>
2026-01-18 00:47:55 -0500

    Basic Setup
    Basic setup. Assets are on screen & in position.

--- Hocus Purrr-cus/Hocus Purr-cus.py
+++ Hocus Purrr-cus/Hocus Purr-cus.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 pygame.init()
 pygame.display.set_caption('Hocus Purr-cus')
@@ -10,6 +11,26 @@ clock = pygame.time.Clock()
 
 bg_surface = pygame.image.load("C:/Users/haley/OneDrive/Documents/GitHub/HocusPurrcus/Hocus Purrr-cus/Graphics/background.png").convert_alpha()
 
+# Player  -------------------------------------------------
+
+player_surf = pygame.image.load("C:/Users/haley/OneDrive/Documents/GitHub/HocusPurrcus/Hocus Purrr-cus/Graphics/player.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (400, 533))
+
+# Player HP -----------------------------------------------
+
+player_HP0 = pygame.image.load("C:/Users/haley/OneDrive/Documents/GitHub/HocusPurrcus/Hocus Purrr-cus/Graphics/HP.png").convert_alpha()
+player_HP0_rect = player_surf.get_rect(center = (300, 200))
+
+# Wizard  -------------------------------------------------
+
+wizard2_surf = pygame.image.load("C:/Users/haley/OneDrive/Documents/GitHub/HocusPurrcus/Hocus Purrr-cus/Graphics/wizard_2.png").convert_alpha()
+wizard2_rect = wizard2_surf.get_rect(midbottom = (400, 460))
+
+# Wizard HP -----------------------------------------------
+
+wizard_HP0 = pygame.transform.flip(player_HP0, 1, 0)
+wizard_HP0_rect = player_surf.get_rect(center = (740, 200))
+
 
 while True:
     for event in pygame.event.get():
@@ -18,5 +39,11 @@ while True:
             exit()
     
     screen.blit(bg_surface, (0,0))
+    screen.blit(wizard2_surf, wizard2_rect)
+    screen.blit(player_surf, player_rect)
+    screen.blit(player_HP0, player_HP0_rect)
+    screen.blit(wizard_HP0, wizard_HP0_rect)
+
+
     pygame.display.update()
     clock.tick(60)
